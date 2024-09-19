bc8bf9ac65b0caf9a16aafd3aff8692e21ae7607
gaisdav <gais.davletkildin@gmail.com>
2024-09-19 21:19:14 +0200

    added github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,59 +3,63 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
-export default defineConfig({
-  server: {
-    open: true,
-    port: 5001,
-  },
-  envPrefix: 'BOOK',
-  base: '/book-diary/',
-  plugins: [
-    react(),
-    svgr({
-      svgrOptions: {
-        exportType: 'default',
-        ref: true,
-        svgo: false,
-        titleProp: true,
-      },
-      include: '**/*.svg',
-    }),
-    VitePWA({
-      registerType: 'autoUpdate',
-      injectRegister: false,
+export default defineConfig(({ mode }) => {
+  const isDev = mode === 'development';
 
-      pwaAssets: {
-        disabled: false,
-        config: true,
-      },
+  return {
+    server: {
+      open: true,
+      port: 5001,
+    },
+    envPrefix: 'BOOK',
+    base: isDev ? '/' : '/book-diary/',
+    plugins: [
+      react(),
+      svgr({
+        svgrOptions: {
+          exportType: 'default',
+          ref: true,
+          svgo: false,
+          titleProp: true,
+        },
+        include: '**/*.svg',
+      }),
+      VitePWA({
+        registerType: 'autoUpdate',
+        injectRegister: false,
 
-      manifest: {
-        name: 'bookbox',
-        short_name: 'bookbox',
-        description: 'bookbox',
-        theme_color: '#ffffff',
-      },
+        pwaAssets: {
+          disabled: false,
+          config: true,
+        },
 
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
-        cleanupOutdatedCaches: true,
-        clientsClaim: true,
-      },
+        manifest: {
+          name: 'bookbox',
+          short_name: 'bookbox',
+          description: 'bookbox',
+          theme_color: '#ffffff',
+        },
 
-      devOptions: {
-        enabled: false,
-        navigateFallback: 'index.html',
-        suppressWarnings: true,
-        type: 'module',
-      },
-    }),
-  ],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        silenceDeprecations: ['legacy-js-api'],
+        workbox: {
+          globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+          cleanupOutdatedCaches: true,
+          clientsClaim: true,
+        },
+
+        devOptions: {
+          enabled: false,
+          navigateFallback: 'index.html',
+          suppressWarnings: true,
+          type: 'module',
+        },
+      }),
+    ],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          silenceDeprecations: ['legacy-js-api'],
+        },
       },
     },
-  },
+  };
 });
