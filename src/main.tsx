32294d88ea0864a5af9b088862ff25a41d6b6240
gaisdav <gais.davletkildin@gmail.com>
2024-09-20 18:33:05 +0200

    added theme changing for status bar

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.scss";
-import "./variables.scss";
-import { RouterProvider } from "react-router-dom";
-import PWABadge from "./PWABadge.tsx";
-import { router } from "./routes/router.tsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.scss';
+import { RouterProvider } from 'react-router-dom';
+import PWABadge from './PWABadge.tsx';
+import { router } from './routes/router.tsx';
 
-const root = document.getElementById("root");
+const root = document.getElementById('root');
 
 if (root) {
   ReactDOM.createRoot(root).render(
@@ -16,5 +15,5 @@ if (root) {
     </React.StrictMode>,
   );
 } else {
-  throw new Error("Root element not found");
+  throw new Error('Root element not found');
 }
