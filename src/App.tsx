2c8ec8e7c8a7cae6d7c149468320cde1682ac6d7
gaisdav <gais.davletkildin@gmail.com>
2024-12-04 15:56:30 +0100

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import { FC, useEffect, useState } from 'react';
+import { FC, useEffect } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { initRouter } from '@/routes/router.tsx';
 import PWABadge from '@/PWABadge.tsx';
 import { Providers } from '@/Providers.tsx';
 import { useProfileStore } from '@/stores/profile/useProfileStore.tsx';
-import type { Router as RemixRouter } from '@remix-run/router/dist/router';
+import { IUser } from '@/data/user/enitites/user';
 
 export const App: FC = () => {
-  const [router, setRouter] = useState<RemixRouter | null>(null);
   const initProfile = useProfileStore().initProfile;
   const profile = useProfileStore().profile;
 
@@ -15,17 +14,17 @@ export const App: FC = () => {
     initProfile();
   }, [initProfile]);
 
-  useEffect(() => {
-    if (!profile) {
-      return;
-    }
-    const router = initRouter(profile);
-    setRouter(router);
-  }, [profile]);
+  const getRouter = (profile: IUser) => {
+    return initRouter(profile);
+  };
 
   return (
     <Providers>
-      {router ? <RouterProvider router={router} /> : 'Initializing...'}
+      {profile ? (
+        <RouterProvider router={getRouter(profile)} />
+      ) : (
+        'Initializing...'
+      )}
       <PWABadge />
     </Providers>
   );
