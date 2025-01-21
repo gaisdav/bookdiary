a9ba80b0431c1afbe9064f8203e45256c5d738b2
g.davletkildin <gais.davletkildin@gmail.com>
2025-01-21 15:33:56 +0100

    fixed forgot password page redirection

--- src/ui/pages/forgotPassword/index.tsx
+++ src/ui/pages/forgotPassword/index.tsx
@@ -1,18 +1,19 @@
-import { FC, FormEventHandler } from 'react';
+import { FC, FormEventHandler, useState } from 'react';
 import { PageWrapper } from '@/ui/components/PageWrapper';
 import { useProfileStore } from '@/stores/profile/useProfileStore.tsx';
 import { Button } from '@/ui/components/ui/button.tsx';
 import { Input } from '@/ui/components/ui/input.tsx';
-import { useNavigate } from 'react-router-dom';
+import { Alert, AlertTitle } from '@/ui/components/ui/alert.tsx';
+import { NavLink } from 'react-router-dom';
 import { ROUTE } from '@/routes/routes.ts';
 
 export const ForgotPassword: FC = () => {
+  const [linkSent, setLinkSent] = useState(false);
+
   const resetPassword = useProfileStore().resetPassword;
   const loading = useProfileStore().resetPasswordLoading;
   const error = useProfileStore().errors.resetPasswordError;
   const resetErrors = useProfileStore().resetErrors;
-
-  const navigate = useNavigate();
 
   const submit: FormEventHandler<HTMLFormElement> = async (event) => {
     event.preventDefault();
@@ -21,9 +22,7 @@ export const ForgotPassword: FC = () => {
     const email = formData.get('email') as string;
 
     await resetPassword(email);
-    navigate(ROUTE.LOGIN, {
-      replace: true,
-    });
+    setLinkSent(true);
   };
 
   const resetError = () => {
@@ -36,23 +35,37 @@ export const ForgotPassword: FC = () => {
       showBack
       contentClassName="items-center"
     >
-      <form
-        onSubmit={submit}
-        className="flex flex-col gap-4 w-full max-w-md"
-        onChange={resetError}
-      >
-        <Input type="email" name="email" required placeholder="Email" />
-        {error && <div className="text-red-800">{error}</div>}
-        <Button type="submit">
-          Reset password
-          {loading && (
+      <div className="flex flex-col gap-4 w-full max-w-md">
+        <form
+          onSubmit={submit}
+          onChange={resetError}
+          className="flex flex-col gap-4 w-full items-center"
+        >
+          <Input type="email" name="email" required placeholder="Email" />
+          {error && <div className="text-red-800">{error}</div>}
+          <Button type="submit">
+            Reset password
+            {loading && (
+              <>
+                {' '}
+                <div className="animate-spin rounded-full border-2 border-gray-300 border-t-gray-900 h-4 w-4" />
+              </>
+            )}
+          </Button>
+        </form>
+        {linkSent && (
+          <Alert className="flex gap-4 items-center">
             <>
-              {' '}
-              <div className="animate-spin rounded-full border-2 border-gray-300 border-t-gray-900 h-4 w-4" />
+              <AlertTitle>
+                We have sent you an email with a link to reset your password.
+              </AlertTitle>
+              <Button variant="outline" size="sm" asChild>
+                <NavLink to={ROUTE.LOGIN}>Sign in</NavLink>
+              </Button>
             </>
-          )}
-        </Button>
-      </form>
+          </Alert>
+        )}
+      </div>
     </PageWrapper>
   );
 };
