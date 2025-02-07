eba8df1a1c595ed1b56c5c2fd4128325c716e997
g.davletkildin <gais.davletkildin@gmail.com>
2025-02-07 14:13:06 +0100

    added env secrets

--- src/lib/supabase.config.ts
+++ src/lib/supabase.config.ts
@@ -1,5 +1,12 @@
 import { createClient } from '@supabase/supabase-js';
 
+if (
+  !import.meta.env.BOOK_SUPABASE_URL ||
+  !import.meta.env.BOOK_SUPABASE_ANON_KEY
+) {
+  throw new Error('Supabase URL and Anon Key are required');
+}
+
 export const supabase = createClient(
   import.meta.env.BOOK_SUPABASE_URL.toString(),
   import.meta.env.BOOK_SUPABASE_ANON_KEY.toString(),
