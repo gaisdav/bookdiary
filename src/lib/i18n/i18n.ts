b8d1d1287b43e0e7d0abe67b08e4a55e207162ed
g.davletkildin <gais.davletkildin@gmail.com>
2025-02-13 20:01:26 +0100

    fix i18next configuration: dynamically set language using GET_LNG function

--- src/lib/i18n/i18n.ts
+++ src/lib/i18n/i18n.ts
@@ -2,9 +2,10 @@ import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import resources from '@/lib/i18n/locales';
 
+const lng = GET_LNG();
+
 i18next.use(initReactI18next).init({
-  //TODO add language detector
-  lng: 'en', // if you're using a language detector, do not define the lng option
+  lng,
   debug: true,
   resources,
 });
